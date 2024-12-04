8a8d6def5aaaae9d527d892bf6fb646de93d8c53
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2024-12-04 14:59:01 +0700

    adding to allow cors

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::options('{any}', function () {
+    return response()->json([], 204, [
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+    ]);
+})->where('any', '.*');
+
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 Route::apiResource('data',DataController::class);
 Route::get('sum',[DataController::class,'sum']);
 Route::put('setlunas/{id}',[DataController::class,'setLunas']);
